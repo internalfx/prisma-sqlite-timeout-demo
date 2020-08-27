0d8d388ad6712683ed6f7b017fc1f4e3487193a6
Bryan Morris <bryan@internalfx.com>
2020-08-27 10:41:10 -0700

    Update main.ts
    Add upsert statement.

--- main.ts
+++ main.ts
@@ -2,9 +2,25 @@ import { PrismaClient } from '@prisma/client'
 
 async function main() {
   const prisma = new PrismaClient()
-
-  const data = await prisma.user.findMany()
+  
+  let data = await prisma.user.findMany()
   console.log(data)
+
+  await prisma.user.upsert({
+    where: { email: 'test@email.com' },
+    create: {
+      name: `${Date.now()}`,
+      email: 'test@email.com'
+    },
+    update: {
+      name: `${Date.now()}`,
+      email: 'test@email.com'
+    }
+  })
+
+  data = await prisma.user.findMany()
+  console.log(data)
+  
   prisma.$disconnect()
 }
 
